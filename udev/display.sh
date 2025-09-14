3c5393c8a13a0665d57ec06e6739c02ca6f903f0
JimmyChin946 <Judes946@gmail.com>
2025-09-13 17:36:39 -0700

    I don't know what is going on

--- udev/display.sh
+++ udev/display.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# exec autorandr clone-largest
+
 if [ "$XDG_SESSION_TYPE" == 'wayland' ]; then
 	# I don't know... 
 	echo "WARNING: wayland display.sh not implemented yet..."
 elif [ "$XDG_SESSION_TYPE" == 'x11' ]; then
+	# autorandr handles everything but the raw hdmi output
+	# for this I want to default to autorandr clone-largest
 	exec autorandr clone-largest
-	exec autorandr --cycle
 fi
 
